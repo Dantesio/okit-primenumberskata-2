a863f207811c220c8d25f557f352b1283f762b2c
Dantesio <55356084+Dantesio@users.noreply.github.com>
2022-12-20 13:14:07 +0300

    Initial commit

---
+++ src/main/java/ru/hse/tdd/PrimeFactors.java
@@ -0,0 +1,8 @@
+package ru.hse.tdd;
+
+public class PrimeFactors {
+
+//    public static boolean of(int number) {
+//        return false;
+//    }
+}
---
+++ src/test/java/ru/hse/tdd/PrimeFactorsShould.java
@@ -0,0 +1,6 @@
+package ru.hse.tdd;
+
+public class PrimeFactorsShould {
+
+
+}
